c67afff92df16256f2201f932620102ce08c12c8
Prem <premdandy@gmail.com>
2021-01-12 17:46:02 +0530

    Shoe List screen scrollable live data implemented

--- app/src/main/java/com/udacity/shoestore/screens/ShoeListFragment.kt
+++ app/src/main/java/com/udacity/shoestore/screens/ShoeListFragment.kt
@@ -4,10 +4,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.udacity.shoestore.R
 import com.udacity.shoestore.databinding.FragmentShoeListBinding
+import com.udacity.shoestore.viewmodel.ShoeListViewModel
 
 
 /**
@@ -15,7 +20,8 @@ import com.udacity.shoestore.databinding.FragmentShoeListBinding
  */
 class ShoeListFragment : Fragment() {
 
-    private lateinit var binding:FragmentShoeListBinding
+    private lateinit var binding: FragmentShoeListBinding
+    private lateinit var shoeListViewModel: ShoeListViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,8 +32,26 @@ class ShoeListFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        binding=DataBindingUtil.inflate(inflater,R.layout.fragment_shoe_list,container,false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_shoe_list, container, false)
+        shoeListViewModel = ViewModelProvider(requireActivity()).get(ShoeListViewModel::class.java)
         return binding.root
+    }
+
+    override fun onStart() {
+        super.onStart()
+        shoeListViewModel.shoeList.observe(viewLifecycleOwner, Observer {
+            for (i in 0..it.size - 1) {
+                val textView = TextView(requireContext())
+                val params = LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.MATCH_PARENT,
+                    LinearLayout.LayoutParams.WRAP_CONTENT
+                )
+                textView.layoutParams = params
+                textView.setText("${it[i].name}")
+                textView.textSize = 26.0F
+                binding.linearLayout.addView(textView)
+            }
+        })
     }
 
 }
--- app/src/main/java/com/udacity/shoestore/viewmodel/ShoeListViewModel.kt
+++ app/src/main/java/com/udacity/shoestore/viewmodel/ShoeListViewModel.kt
@@ -3,11 +3,21 @@ package com.udacity.shoestore.viewmodel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.udacity.shoestore.R
 import com.udacity.shoestore.models.Shoe
 
 class ShoeListViewModel: ViewModel() {
 
-    private val _shoeList=MutableLiveData<Shoe>()
-    val shoeList:LiveData<Shoe>
-    get() = _shoeList
+    private val _shoeList=MutableLiveData<List<Shoe>>()
+    val shoeList:LiveData<List<Shoe>>
+        get() = _shoeList
+
+    init {
+        _shoeList.postValue(mutableListOf(Shoe("The Walker", 10.00, "Nike", R.string.nike),
+            Shoe("Jogger", 9.00, "Reebok", R.string.reebok),
+            Shoe("Racer", 10.00, "Nike", R.string.reebok),
+            Shoe("Cool Step", 10.00, "Nike", R.string.nike)))
+    }
+
+
 }
